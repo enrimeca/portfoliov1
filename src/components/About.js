ed8e0e40b2872a6e187c85fb4ae82886c7302770
Enrique <enriquemejiacano@gmail.com>
2020-08-23 17:50:30 -0500

    about 2

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import '../assets/styles/components/About.scss';
 import about from '../assets/images/about.jpg'
 
 export class About extends Component {
@@ -13,12 +14,24 @@ export class About extends Component {
             </div>
           </div>
           <div className="row">
-            <div className="col m-3">
+            <div className="col mx-3">
               <p>
               I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between. My goal is to always build products that provide pixel-perfect, performant experiences.
               Shortly after graduating from Northeastern University, I joined the engineering team at Upstatement where I work on a wide variety of interesting and meaningful projects on a daily basis.
               Here are a few technologies I've been working with recently:
-              </p>
+              </p>               
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <ul className='skills-list'>
+                  <li>-JavaScript (ES6+)</li>                
+                  <li>-HTML & (S)CSS</li>
+                  <li>-React</li>                
+                  <li>-Bootstrap</li>
+                  <li>-Node.js</li>
+                  <li>-Gatsby</li>
+              </ul>   
             </div>
           </div>
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,15 +9,15 @@ export class Header extends Component {
         <div className="container mt-2 mb-2 section-inner">
           <div className="row h-100">
             <div className="col-md-6 col-12">
-            <img class="profile-image img-fluid rounded-circle mx-auto d-block pb-3" src={photo} alt="profile image" />
+            <img className="profile-image img-fluid rounded-circle mx-auto d-block pb-3" src={photo} alt="profile image" />
             </div>
             <div className="col-md-6 col-12 my-auto">
               <div className="container">
                 <h4 className='title'>Hi, my name is</h4>
                 <h1 className='name'>Enrique Mejía</h1>
                 <p className='description'>I'm a software engineer based in Piura, Perú specializing in building and designing websites with Javascript and other technologies.</p>
-                <a class="btn btn-linkedin mr-2" href="https://www.linkedin.com/in/enriquemejiacano/" role="button" target="_blank">Linkedin</a>  
-                <a class="btn btn-resume" href="#" role="button" target="_blank">Resume</a>  
+                <a className="btn btn-linkedin mr-2" href="https://www.linkedin.com/in/enriquemejiacano/" role="button" target="_blank">Linkedin</a>  
+                <a className="btn btn-resume" href="#" role="button" target="_blank">Resume</a>  
               </div>               
             </div>
           </div>
